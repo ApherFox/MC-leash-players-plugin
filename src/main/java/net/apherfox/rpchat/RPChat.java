9499fec6d003dae6060339dd337e2ddf0f1c9c75
ApherFox <40737070+ApherFox@users.noreply.github.com>
2018-07-02 17:37:29 +0000

    Update RPChat.java

--- src/main/java/net/apherfox/rpchat/RPChat.java
+++ src/main/java/net/apherfox/rpchat/RPChat.java
@@ -1,95 +1,15 @@
 package net.apherfox.rpchat;
 
-import org.bstats.bukkit.Metrics;
-import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.Javaplugin;
 
-// import net.apherfox.rpchat.<module>
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.logging.Level;
-
-public class RPChat extends JavaPlugin {
-
-	private File messagesFile = new File(this.getDataFolder(), "messages.yml");
-	private FileConfiguration messages;
-
-	private File settingsFile = new File(this.getDataFolder(), "config.yml");
-	private FileConfiguration settings;
-
-	private boolean mc18 = this.getServer().getVersion().contains("1.8");
-
-	private Metrics metrics;
-
+public final class rpchat extends JavaPlugin {
 	@Override
-	public FileConfiguration getConfig() {
-		return settings;
+	public void onEnabled() {
+		getLogger().info("onEnable has been invoked!");
 	}
-
-	public FileConfiguration getMessages() {
-		return messages;
-	}
-
-	public Boolean isAboveMC18() {
-		return !mc18;
-	}
-
-	public FileConfiguration getSettings() {
-		return settings;
-	}
-
+	
 	@Override
-	public void reloadConfig() {
-
-		messages = YamlConfiguration.loadConfiguration(messagesFile);
-		settings = YamlConfiguration. loadConfiguration(settingsFile);
-
-		addMessageDefaults();
-		addSettingsDefaults();
-	}
-
-	@Override
-	public void onEnable() {
-
-	if (!isAboveMC18()) {
-		getLogger().info("[RPChat] Minecraft versions before 1.9 are not supported!");
-		getServer().getPluginManager().disablePlugin(this);
-		return;
-	}
-
-	createConfigs();
-	reloadConfigs();
-
-	PluginManager pm = getServer().getPluginManager();
-	//pm.registerEvents(new <EVENT>(this), (this);
-
-	getCommand("rpchat").setExecutor(new Executor(this));
-
-	boolean checkUpdates = getSettings().getBoolean("check-updates");
-	if (checkUpdates) {
-		new Thread(() -> new Updater(getDescription()).checkCurrentVersion()).start();
-	}
-
-	if (getSettings().getBoolean("allow-metrics")) {
-		metrics = new Metrics(this);
-		getLogger().info("Metrics successfully initialized!");
-
-	} else {
-		getLogger().warning("Metrics disabled");
+	public void onDisable() {
+		getLogger().info("onDisable has been invoked!");
 	}
 }
-
-private int addMessage(String node, String message) {
-	if (messages.get(node) == null) {
-		messages.set(node, message);
-		return 1;
-	}
-	return 0;
-}
-
-private void addMessageDefaults() {
