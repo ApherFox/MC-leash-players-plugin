2ad1e131cc60040306e257779a219cf495518b19
ApherFox <40737070+ApherFox@users.noreply.github.com>
2018-07-11 15:55:57 +0000

    Create InventoryClick.java

---
+++ src/main/java/net/apherfox/random-proj/events/InventoryClick.java
@@ -0,0 +1,18 @@
+package events;
+
+import java.util.ArrayList;
+
+public class InventoryClick implements Listener {
+  @EventHandler
+  public void onInventoryClick (InventoryClickEvent event) {
+    ArrayList<GUI> later = new ArrayList();
+    for (GUI gui : GUI.guis) {
+      if (gui.inventory.equals(event.getInventory())) {
+        later.add(gui);
+      }
+    }
+    for (GUI gui : later) {
+      gui.onInventoryClick(event);
+    }
+  }
+}
